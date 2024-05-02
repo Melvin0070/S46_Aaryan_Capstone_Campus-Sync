a1890d4d729aedc38d94316ff46e86e68aab95fa
aaryan <aaryanpanda04@gmail.com>
2024-05-02 17:48:33 +0530

    Connected server with mongoose database

--- Backend/server.js
+++ Backend/server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/ping', (req, res) => {
-    res.json({ message: 'pong' })
+const {connectToDB, isConnected} = require("./database.js");
+
+
+app.get('/status', (req, res) => {
+    res.json({
+        message: 'pong',
+        database: isConnected() ? 'connected' : 'not connected'
+    })
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
+    await connectToDB();
     console.log(`🚀 server running on PORT: ${port}`);
 });
