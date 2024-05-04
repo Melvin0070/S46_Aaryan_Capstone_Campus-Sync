78a99ee6d41af340199c89faee255156d6c44d2b
aaryan <aaryanpanda04@gmail.com>
2024-05-04 23:54:07 +0530

    created post routes for the user (signup, login)

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -3,8 +3,10 @@ import * as userController from "../controllers/userController.js";
 
 
 const router = express.Router();
-const { getUserData } = userController;
+const { getUserData, createUser, loginUser } = userController;
 
 router.get("/details", getUserData);
+router.get("/signup", createUser);
+router.get("/login", loginUser);
 
 export default router;
