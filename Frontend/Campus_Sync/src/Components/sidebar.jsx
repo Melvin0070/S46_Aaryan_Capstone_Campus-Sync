b1ddbc8d66692e4928fcac36fa4ebd525b4cde23
aaryan <aaryanpanda04@gmail.com>
2024-05-22 11:27:29 +0530

    added icons to the sidebar

--- Frontend/Campus_Sync/src/Components/sidebar.jsx
+++ Frontend/Campus_Sync/src/Components/sidebar.jsx
@@ -1,18 +1,54 @@
 import React from "react";
 import "./sidebar.css";
+import home from "../assets/home.png";
+import result from "../assets/results.png";
+import fee from "../assets/fee.png";
+import drops from "../assets/drops.png";
+import help from "../assets/helpdesk.png";
+import logout from "../assets/logout.jpg";
 
 function Sidebar() {
   return (
     <div id="sidebar">
       <div id="sidebar-buttons-div">
-        <div className="sidebar-buttons">Dashboard</div>
-        <div className="sidebar-buttons">Results</div>
-        <div className="sidebar-buttons">Fee Breakdown</div>
-        <div className="sidebar-buttons">Drops</div>
-        <div className="sidebar-buttons">Helpdesk</div>
+        <div className="sidebar-buttons">
+          <div className="sidebar-img-p">
+            <img src={home} alt="home" />
+            <p>Dashboard</p>
+          </div>
+        </div>
+        <div className="sidebar-buttons">
+          <div className="sidebar-img-p">
+            <img src={result} alt="result" />
+            <p>Results</p>
+          </div>
+        </div>
+        <div className="sidebar-buttons">
+          <div className="sidebar-img-p">
+            <img src={fee} alt="fee" />
+            <p>Fee Breakdown</p>
+          </div>
+        </div>
+        <div className="sidebar-buttons">
+        <div className="sidebar-img-p">
+            <img src={drops} alt="drops" />
+            <p>Drops</p>
+          </div>
+        </div>
+        <div className="sidebar-buttons">
+          <div className="sidebar-img-p">
+            <img src={help} alt="help" />
+            <p>Helpdesk</p>
+          </div>
+        </div>
       </div>
-      <div >
-        <div className="sidebar-buttons" id="logout-div">Logout</div>
+      <div>
+        <div className="sidebar-buttons" id="logout-div">
+        <div className="sidebar-img-p">
+            <img id="logout-img" src={logout} alt="logout" />
+            <p>Logout</p>
+          </div>
+        </div>
       </div>
     </div>
   );
