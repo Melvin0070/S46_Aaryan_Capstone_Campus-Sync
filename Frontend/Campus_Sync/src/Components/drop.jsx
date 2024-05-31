5fb1dac5c73bec5fc2905447f9808185bbb9bd1c
aaryan <aaryanpanda04@gmail.com>
2024-05-31 22:20:58 +0530

    created multiple components inside drops page

---
+++ Frontend/Campus_Sync/src/Components/drop.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './drop.css';
+
+function Drop() {
+  return (
+    <div>Drop</div>
+  )
+}
+
+export default Drop;
--- Frontend/Campus_Sync/src/Pages/Drops.jsx
+++ Frontend/Campus_Sync/src/Pages/Drops.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './Drops.css';
+import Navbar from '../Components/navbar';
+import Sidebar from '../Components/sidebar';
+import Hamburger from '../Components/hamburger';
+import useSidebar from '../Components/toggleSidebar';
+import Drop from '../Components/drop';
 
-function Drops() {
-  return (
-    <div>Drops</div>
-  )
+function Fees() {
+    const { isSidebarVisible, toggleSidebar } = useSidebar();
+ 
+    return (
+      <>
+        <Navbar />
+        <div id="sidebar-divs-container">
+          {isSidebarVisible && <Sidebar/>}
+          <div id="hamburger-result-container">
+            <Hamburger name="Drops" toggleSidebar={toggleSidebar} />
+            <Drop/>
+          </div>
+        </div>
+      </>
+    );
 }
 
-export default Drops;
+export default Fees;
